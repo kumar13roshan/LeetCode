57d54ea50eb091be800c7365934586503aeb05de
Roshan Kumar <kumarroshan39620@gmail.com>
2026-08-31 16:16:19 +0530

    Time: 3 ms (69.41%), Space: 48.9 MB (80.15%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int n=arr.length;
-        for(int i=0;i<n-1;i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=i+1;j<n;j++){
-                max=Math.max(max,arr[j]);
-            }
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--){
+            int current =arr[i];
             arr[i]=max;
+            max=Math.max(max,current);
         }
-        arr[n-1]=-1;
         return arr;
     }
 }
